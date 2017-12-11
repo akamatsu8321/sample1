51ed8b18e2dd5b6a04ec903a3a396588304bee11
tatsunoriishii <ishiidev22@gmail.com>
2017-12-11 15:19:34 +0900

    a

--- src/com/internousdev/sample1/action/UserCreateCompleteAction.java
+++ src/com/internousdev/sample1/action/UserCreateCompleteAction.java
@@ -1,11 +1,61 @@
 package com.internousdev.sample1.action;
 
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.sample1.dao.UserCreateCompleteDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class UserCreateCompleteAction extends ActionSupport{
+public class UserCreateCompleteAction extends ActionSupport implements SessionAware{
 
-	public String execute(){
-		return SUCCESS;
+	private String loginUserId;
+
+	private String loginPassword;
+
+	private String userName;
+
+	public Map<String,Object> session;
+
+	private String result;
+
+	private UserCreateCompleteDAO userCreateCompleteDAO = new UserCreateCompleteDAO();
+
+	public String execute() throws SQLException {
+
+		userCreateCompleteDAO.cerateUser(session.get("loginUserId").toString(),
+				session.get("loginPassword").toString(),
+				session.get("userName").toString());
+
+		result = SUCCESS;
+
+		return result;
+	}
+
+	public String getLoginUserId(){
+		return loginUserId;
+	}
+	public void setLoginUserId(String loginUserId){
+		this.loginUserId = loginUserId;
+	}
+
+	public String getLoginPassword(){
+		return loginPassword;
+	}
+	public void setLoignPassword(String loginPassword){
+		this.loginPassword=loginPassword;
+	}
+
+	public String getUserName(){
+		return userName;
+	}
+	public void setUserName(String userName){
+		this.userName=userName;
+	}
+
+	public void setSession(Map<String,Object> session){
+		this.session=session;
 	}
 
 }
