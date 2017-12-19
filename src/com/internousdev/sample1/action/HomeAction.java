e228afe952edc30d8dd361e7b2f674e2e93bc5dc
internousdev <internousdev@103163794>
2017-12-19 17:44:11 +0900

    12/19 4

--- src/com/internousdev/sample1/action/HomeAction.java
+++ src/com/internousdev/sample1/action/HomeAction.java
@@ -1,24 +1,73 @@
+
 package com.internousdev.sample1.action;
 
-import com.internousdev.sample1.dao.BuyItemDAO;
-import com.internousdev.sample1.dto.BuyItemDTO;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HomeAction extends ActionSupport implements SessionAeare {
-	
-	public Map<String, Object> session;
-	
-	public String execute(){
-		
-		BuyItemDAO buyItemDAO = new BuyItemDAO();
-		BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
-		session.put("id", buyItemDTO.getId());
-		session.put("buyItem_name", buyItemDTO.getItemName());
-		session.put("buyItem_price", buyItemDTO.getItemPrice());
+public class HomeAction extends ActionSupport implements SessionAware {
 
-		
-		return SUCCESS;
-		
+	/**
+	 * アイテム購入個数
+	 */
+	private int count;
+
+	/**
+	 * 支払い方法
+	 */
+	private String pay;
+
+	/**
+	 * アイテム情報を格納
+	 */
+	public Map<String, Object>  session;
+
+	/**
+	 * 商品情報取得メソッド
+	 *
+	 * @author internous
+	 */
+	public String execute() {
+		String result = SUCCESS;
+		session.put("count", count);
+		int intCount = Integer.parseInt(session.get("count").toString());
+		int intPrice = Integer.parseInt(session.get("buyItem_price").toString());
+
+		session.put("total_price", intCount * intPrice);
+		String payment;
+
+		if(pay.equals("1")) {
+
+			payment = "現金払い";
+			session.put("pay", payment);
+		} else {
+
+			payment = "クレジットカード";
+			session.put("pay", payment);
+		}
+		return result;
 	}
 
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getPay() {
+		return pay;
+	}
+
+	public void setPay(String pay) {
+		this.pay = pay;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
 }
